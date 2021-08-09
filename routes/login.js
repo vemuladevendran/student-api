a5d94ee5ed5ac770ceda0f5ba68409a1082a865b
devendran <devendranvemula@gmail.com>
2021-08-09 20:25:18 +0530

    clean up

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,4 @@
 const { login } = require('../controllers/login');
-const { checkToken } = require('../services/auth')
 module.exports = function loginRoutes(app) {
-    app.post('/api/v1/login', checkToken,login);
+    app.post('/api/v1/login',login);
 }
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const { createUser, updateUser, deleteUser, getUsers, getUserById, verifyUser } 
 
 const { checkToken } = require('../services/auth')
 module.exports = function userRoutes(app) {
-    app.post('/api/v1/users', createUser);
-    // app.post('/api/v1/users', checkToken, createUser);
+    // app.post('/api/v1/users', createUser);
+    app.post('/api/v1/users', checkToken, createUser);
     app.get('/api/v1/users', checkToken, getUsers);
     app.get('/api/v1/users/:id', checkToken, getUserById);
     app.put('/api/v1/users/:id', checkToken, updateUser);
