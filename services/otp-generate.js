9cffdae085b9ee8e4b3506fc04fbc5e7ff539a24
devendran <devendranvemula@gmail.com>
2021-08-28 08:10:29 +0530

    clean up

--- services/otp-generate.js
+++ services/otp-generate.js
@@ -1,49 +1,53 @@
-'use strict'
+"use strict";
 
-const Users = require('../models/users');
-const otpGenerator = require('otp-generator');
-const Otp = require('../models/otp');
-
+const Users = require("../models/users");
+const otpGenerator = require("otp-generator");
+const Otp = require("../models/otp");
 
 const generateOtp = async (id) => {
-    try {
-        const otpDoc = await Otp.create({
-            otp: otpGenerator.generate(6, { upperCase: false, alphabets: false, specialChars: false }),
-            userId: id,
-        });
+  try {
+    const otpDoc = await Otp.create({
+      otp: otpGenerator.generate(6, {
+        upperCase: false,
+        alphabets: false,
+        specialChars: false,
+      }),
+      userId: id,
+    });
 
-        return otpDoc.otp;
-    } catch (error) {
-        console.error(error);
-    }
+    return otpDoc.otp;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const verifyOtp = async (id, otp) => {
-
-    try {
-        const otpObj = await Otp.findOne({ userId: id });
-        // console.log(otpObj)
-        if (!otpObj) {
-            return false;
-        };
-
-        if (otpObj.otp === otp) {
-            // exceute
-            const result = await Users.findOneAndUpdate(id, { isEmailVerified: true });
-            const removeOtp = await Otp.findOneAndRemove(id);
-            return true;
-        };
-
-        return false;
-
-    } catch (error) {
-        console.error(error);
-        return false;
+  try {
+    const otpObj = await Otp.findOne({ userId: id });
+    // console.log(otpObj)
+    if (!otpObj) {
+      return false;
     }
 
-}
+    if (otpObj.otp === otp) {
+      // exceute
+      const result = await Users.findOneAndUpdate(
+        { id: id },
+        { isEmailVerified: true },
+        { new: true }
+      );
+      const removeOtp = await Otp.findOneAndRemove(id);
+      return true;
+    }
+
+    return false;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
 
 module.exports = {
-    generateOtp,
-    verifyOtp
-}
+  generateOtp,
+  verifyOtp,
+};
